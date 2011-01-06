c269bc2d587eb7fa11061226e22180084400a4e2
David Boyce <boyski@users.sourceforge.net>
2011-01-06 10:44:04 -0500

    Added copyright and GPL clauses.

--- libB/libB.h
+++ libB/libB.h
@@ -1,3 +1,35 @@
+#ifndef _LIBC_H
+#define _LIBC_H
+
+/*
+ * Copyright (c) 2002-2010 David Boyce.  All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+extern int c1(void);
+extern int c2(void);
+extern int c3(void);
+
+#endif /*_LIBC_H*/
+#ifndef _LIBA_H
+#define _LIBA_H
+
+extern int a1(void);
+extern int a2(void);
+
+#endif /*_LIBA_H*/
 #ifndef _LIBB_H
 #define _LIBB_H
 
